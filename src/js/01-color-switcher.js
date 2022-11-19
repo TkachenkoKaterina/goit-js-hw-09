5ef599b5f24056c19bd428b40403acd503ec0933
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2022-11-19 15:27:12 +0200

    Task_1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+"use strict";
+
+let timerId = null;
+const bodyRef = document.querySelector('body');
+
+const startBtnRef = document.querySelector('[data-start]');
+
+const stopBtnRef = document.querySelector('[data-stop]');
+stopBtnRef.setAttribute("disabled", true);
+
+const changeColor = startBtnRef.addEventListener('click', onStartBtn);
+
+const onStopBtnClick = stopBtnRef.addEventListener('click', onStopBtn);
+
+
+function onStartBtn() {
+    timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    startBtnRef.setAttribute("disabled", true);
+    stopBtnRef.removeAttribute("disabled", false);
+    }, 1000);
+}
+
+function onStopBtn() {
+    clearInterval(timerId);
+    stopBtnRef.setAttribute("disabled", true);
+    startBtnRef.removeAttribute("disabled", false);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
