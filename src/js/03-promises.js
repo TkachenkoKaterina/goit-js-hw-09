e05e295388a7bbba78c559c0799f27edd326b2eb
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2022-11-20 22:36:25 +0200

    Task_3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,26 +1,47 @@
 "use strict";
 
-console.log(5);
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
 
-// const refs = {
-//   inputFirstDelay: document.querySelector('input'),
-// }
+const refs = {
+    inputFirstDelay: document.querySelector('[name="delay"]'),
+    inputDelayStep: document.querySelector('[name="step"]'),
+    inputAmount: document.querySelector('[name="amount"]'),
+    btnCreatePromise: document.querySelector('form'),
+};
 
-// console.log(refs.inputFirstDelay);
+const btnClick = refs.btnCreatePromise.addEventListener('submit', onBtnClick);
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+function onBtnClick(event) {
+    event.preventDefault();
+    let firstDelay = Number(refs.inputFirstDelay.value);
+    let delayStep = Number(refs.inputDelayStep.value);
+    let amount = Number(refs.inputAmount.value);
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+    for (let i = 1; i <= amount; i += 1) {
+        createPromise(i, firstDelay);
+        firstDelay += delayStep;
+    }
+}
+
+function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (shouldResolve) {
+            Notiflix.Notify.success(resolve(`Fulfilled promise ${position} in ${delay}ms`));
+        } else {
+            Notiflix.Notify.failure(reject(`Rejected promise ${position} in ${delay}ms`));
+        
+        }
+    }, delay);
+    });
+
+    promise
+    .then(value => {
+        console.log(value);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+} 
