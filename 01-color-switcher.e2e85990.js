7468503271832f5a739ee7d18613927d9bed832b
TkachenkoKaterina <90516132+TkachenkoKaterina@users.noreply.github.com>
2022-11-22 20:00:19 +0000

    Deploying to gh-pages from @ TkachenkoKaterina/goit-js-hw-09@3011342b426ae7301acb8469c143995f540b9824 🚀

---
+++ 01-color-switcher.e2e85990.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("body"),r=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.setAttribute("disabled",!0);r.addEventListener("click",(function(){r.setAttribute("disabled",!0),d.removeAttribute("disabled",!1),t=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(function(){clearInterval(t),d.setAttribute("disabled",!0),r.removeAttribute("disabled",!1)}));
+//# sourceMappingURL=01-color-switcher.e2e85990.js.map
